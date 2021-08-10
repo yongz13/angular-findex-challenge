b3e288e27b37156dc34024a7a4c57bd48feebc65
yongz21 <86578768+yongz21@users.noreply.github.com>
2021-08-10 15:50:48 +1000

    destroy subscription

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,9 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { merge } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { merge, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 import { EventsDataSource } from '../core/EventsDataSource';
 import { TicketEventService } from '../core/ticket-event.service';
 
@@ -12,12 +12,13 @@ import { TicketEventService } from '../core/ticket-event.service';
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.scss']
 })
-export class EventListComponent implements OnInit, AfterViewInit {
+export class EventListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   displayedColumns = ['name', 'url', 'info', 'pleaseNote'];
   dataSource!: EventsDataSource;
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  notifier = new Subject()
 
   constructor(private ticketEventService: TicketEventService, public dialog: MatDialog ) {
   }
@@ -29,12 +30,17 @@ export class EventListComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
 
-    this.sort.sortChange.subscribe(() => {this.paginator.pageIndex = 0;});
+    this.sort.sortChange.pipe(takeUntil(this.notifier)).subscribe(() => {this.paginator.pageIndex = 0;});
     merge(this.sort.sortChange, this.paginator.page)
     .pipe(
-        tap(() => this.dataSource.loadEvents(this.paginator.pageIndex))
+        tap(() => this.dataSource.loadEvents(this.paginator.pageIndex)),
+        takeUntil(this.notifier)
     )
     .subscribe();
 
+  }
+  ngOnDestroy() {
+      this.notifier.next()
+      this.notifier.complete()
   }
 }
